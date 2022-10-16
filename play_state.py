5b286a15ff3989353f3667d38829fd0416d1ba05
2021170045 @ laptop <yena5282@naver.com>
2022-10-16 16:37:02 +0900

    play state 오류 수정

--- play_state.py
+++ play_state.py
@@ -3,13 +3,6 @@ import game_framework
 import logo_state
 import title_state
 
-# class Grass:
-#     def __init__(self):
-#         self.image = load_image('grass.png')
-#
-#     def draw(self):
-#         self.image.draw(400, 300)
-
 # class Boy:
 #     def __init__(self):
 #         self.x, self.y = 0, 90
@@ -23,39 +16,81 @@ import title_state
 #     def draw(self):
 #         self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
 
+# class Player1:
+#     def __init__(self):      # player1 방향에 따라 리소스 변경
+#         if p1Right == True:
+#             self.image = load_image('Right.png')
+#         elif p1Left == True:
+#             self.image = load_image('Left.png')
+#         elif p1Up == True:
+#             self.image = load_image('Up.png')
+#         elif p1Down == True:
+#             self.image = load_image('Down.png')
+#     def draw(self):
+#         self.image.draw()
+class Map1:
+    def __init__(self):
+        self.image = load_image('map1.png')
+
+    def draw(self):
+        self.image.draw(600, 450)
+
 
 def handle_events():
+    global p1Right, p1Left, p1Down, p1Up  # player1의 이동 방향
+    global p2Right, p2Left, p2Down, p2Up  # player2의 이동 방향
     global running
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            game_framework.change_state(title_state)
-    delay(0.01)
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                game_framework.change_state(title_state)
+    # delay(0.01)
 
 # boy = None
 # grass = None
-# running = True
+running = True
+map1 = None
+p1Down = True
+p1Up = False
+p1Right = False
+p1Left = False
+
+halfBlockWidth = 30
+halfBlockHeight = 30
+LTX = 0
+LTY = 900
+RBX = 1200
+RBY = 0
 
 # 게임 초기화 코드
-# def enter():
-#     global boy, grass, running
+def enter():
+    global map1, running
     # boy = Boy()
-    # grass = Grass()
-    # running = True
+    map1 = Map1()
+    running = True
 
 # finalization code
-# def exit():
-#     global boy, grass
+def exit():
+    global map1
 #     del boy
-#     del grass
+    del map1
 #
 # def update():
 #     boy.update()
 #
-# def draw():
-#     clear_canvas()
-#     grass.draw()
+def draw():
+    clear_canvas()
+    map1.draw()
 #     boy.draw()
-#     update_canvas()
+    update_canvas()
+
+def update():
+    pass
+
+def pause():
+    pass
+def resume():
+    pass
