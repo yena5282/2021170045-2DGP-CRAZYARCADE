ed92259bce1195c29a974a51848fe7813a27c112
2021170045 @ laptop <yena5282@naver.com>
2022-11-04 03:47:43 +0900

    class 분리 중

--- play_state.py
+++ play_state.py
@@ -1,5 +1,8 @@
 from pico2d import *
 import game_framework
+from map1 import Map1
+from heart import Heart
+
 import logo_state
 import title_state
 
@@ -10,66 +13,66 @@ class Map1Wall:
     def draw(self):
         self.image.draw(wallLTX+30, wallLTY-30)
 
-class Map1:
-    def __init__(self):
-        self.image = load_image('resource/map1.png')
+# class Map1:
+#     def __init__(self):
+#         self.image = load_image('resource/map1.png')
+#
+#     def draw(self):
+#         self.image.draw(600, 450)
 
-    def draw(self):
-        self.image.draw(600, 450)
-
-class Player1:
-    def __init__(self):      # player1 방향에 따라 리소스 변경
-        self.x, self.y = 58, 95    # 플레이어1 처음 위치 : 왼쪽 아래 구석탱이
-        self.frame = 0
-        self.dir = 1  # 오른쪽
-        self.image = load_image('resource/Player1.png')
-
-    def update(self):
-        self.frame = (self.frame + 1) % 5
-        if moving == True:
-            if p1Dir == 1:
-                self.dir = 1
-                self.y += self.dir * 1
-            elif p1Dir == 2:
-                self.dir = -1
-                self.y += self.dir * 1
-            elif p1Dir == 3:
-                self.dir = -1
-                self.x += self.dir * 1
-            elif p1Dir == 4:
-                self.dir = 1
-                self.x += self.dir * 1
-
-        elif moving == False:
-            if p1Dir == 1:
-                self.dir -= 1
-            elif p1Dir == 2:
-                self.dir += 1
-            elif p1Dir == 3:
-                self.dir += 1
-            elif p1Dir == 4:
-                self.dir -= 1
-
-    def draw(self):
-        if moving == True:
-            if p1Dir == 1:
-                self.image.clip_draw(self.frame * p1Width, 210, p1Width, p1Height, self.x, self.y)
-            elif p1Dir == 2:
-                self.image.clip_draw(self.frame * p1Width, 140, p1Width, p1Height, self.x, self.y)
-            elif p1Dir == 3:
-                self.image.clip_draw(self.frame * p1Width, 0, p1Width, p1Height, self.x, self.y)
-            elif p1Dir == 4:
-                self.image.clip_draw(self.frame * p1Width, 70, p1Width, p1Height, self.x, self.y)
-
-        elif moving == False:
-            if p1Dir == 1:
-                self.image.clip_draw(0, 210, p1Width, p1Height, self.x, self.y)
-            elif p1Dir == 2:
-                self.image.clip_draw(0, 140, p1Width, p1Height, self.x, self.y)
-            elif p1Dir == 3:
-                self.image.clip_draw(0, 0, p1Width, p1Height, self.x, self.y)
-            elif p1Dir == 4:
-                self.image.clip_draw(0, 70, p1Width, p1Height, self.x, self.y)
+# class Player1:
+#     def __init__(self):      # player1 방향에 따라 리소스 변경
+#         self.x, self.y = 58, 95    # 플레이어1 처음 위치 : 왼쪽 아래 구석탱이
+#         self.frame = 0
+#         self.dir = 1  # 오른쪽
+#         self.image = load_image('resource/Player1.png')
+#
+#     def update(self):
+#         self.frame = (self.frame + 1) % 5
+#         if moving == True:
+#             if p1Dir == 1:
+#                 self.dir = 1
+#                 self.y += self.dir * 1
+#             elif p1Dir == 2:
+#                 self.dir = -1
+#                 self.y += self.dir * 1
+#             elif p1Dir == 3:
+#                 self.dir = -1
+#                 self.x += self.dir * 1
+#             elif p1Dir == 4:
+#                 self.dir = 1
+#                 self.x += self.dir * 1
+#
+#         elif moving == False:
+#             if p1Dir == 1:
+#                 self.dir -= 1
+#             elif p1Dir == 2:
+#                 self.dir += 1
+#             elif p1Dir == 3:
+#                 self.dir += 1
+#             elif p1Dir == 4:
+#                 self.dir -= 1
+#
+#     def draw(self):
+#         if moving == True:
+#             if p1Dir == 1:
+#                 self.image.clip_draw(self.frame * p1Width, 210, p1Width, p1Height, self.x, self.y, 80, 95)
+#             elif p1Dir == 2:
+#                 self.image.clip_draw(self.frame * p1Width, 140, p1Width, p1Height, self.x, self.y, 80, 95)
+#             elif p1Dir == 3:
+#                 self.image.clip_draw(self.frame * p1Width, 0, p1Width, p1Height, self.x, self.y, 80, 95)
+#             elif p1Dir == 4:
+#                 self.image.clip_draw(self.frame * p1Width, 70, p1Width, p1Height, self.x, self.y, 80, 95)
+#
+#         elif moving == False:
+#             if p1Dir == 1:
+#                 self.image.clip_draw(0, 210, p1Width, p1Height, self.x, self.y, 80, 95)
+#             elif p1Dir == 2:
+#                 self.image.clip_draw(0, 140, p1Width, p1Height, self.x, self.y, 80, 95)
+#             elif p1Dir == 3:
+#                 self.image.clip_draw(0, 0, p1Width, p1Height, self.x, self.y, 80, 95)
+#             elif p1Dir == 4:
+#                 self.image.clip_draw(0, 70, p1Width, p1Height, self.x, self.y, 80, 95)
 
 def handle_events():
      global p1Dir, p1X, p1Y
@@ -132,10 +135,10 @@ def enter():
 
 # finalization code
 def exit():
-    global map1, map1Wall, player1
+    global map1, map1Wall, p1
     del map1Wall
     del map1
-    del player1
+    del p1
 
 def update():
     p1.update()
