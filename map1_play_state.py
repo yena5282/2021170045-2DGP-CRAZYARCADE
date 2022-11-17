b8762420efefd1cf733109fbae119e5aed46185b
2021170045 @ laptop <yena5282@naver.com>
2022-11-17 20:05:55 +0900

    c키로 맵 변경하는 기능 제거
    실수로 자꾸 누르게 돼서 해당 기능 제거함

--- map1_play_state.py
+++ map1_play_state.py
@@ -24,8 +24,8 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
-        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_c):
-            game_framework.change_state(title_state)
+        # elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_c):
+        #     game_framework.change_state(title_state)
         elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
             game_framework.quit()
         else:
